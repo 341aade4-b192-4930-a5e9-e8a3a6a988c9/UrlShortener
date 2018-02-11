69f454aa2018933737ebcfa17a5c0660bdf49c76
pavlovyuriy <yuriy.pavlov@castle.co>
2018-02-11 04:15:38 +0400

    change short url generation

--- app/services/short_url_generator.rb
+++ app/services/short_url_generator.rb
@@ -1,9 +1,15 @@
 class ShortUrlGenerator
+  SHORT_URL_LENGTH = 6
+
   def self.call
+    charset = Array('0'..'9') + Array('a'..'z')
+
     loop do
-      # TODO: limit to retry
-      # TODO: hex -> to ?
-      short_url = SecureRandom.hex(5)
+      short_url =
+          Array.new(SHORT_URL_LENGTH) do
+            charset.sample
+          end.join
+
       break short_url unless Link.exists?(short_url: short_url)
     end
   end
